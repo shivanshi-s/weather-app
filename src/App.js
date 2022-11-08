bffb715415d8c53f992e617fb2a5cf7c99c3de3f
shivanshi-s <shivanshi.saxena2@gmail.com>
2022-11-08 12:49:13 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Search from "./components/search/Search"
+import './App.css';
+
+function App() {
+
+  const  handleOnSearchChange = (searchData) => {
+    console.log(searchData);
+  }
+
+  return (
+    <div className="container">
+      <Search onSearchChange={handleOnSearchChange} />
+    </div>
+  );
+}
+
+export default App;
